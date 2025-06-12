c3d6c9ce474757150c677e8343317265547a18a4
omanand <omanandswami2005@gmail.com>
2025-06-12 19:03:20 +0530

    License added, unrequired component removeed from react build

--- src/react/index.js
+++ src/react/index.js
@@ -1,5 +1,3 @@
 // React-specific exports
 export * from './hooks/use-gemini-live.js';
 export * from './components/GeminiLiveProvider.jsx';
-export * from './components/AudioVisualizer.jsx';
-export * from './components/VoiceChat.jsx';
